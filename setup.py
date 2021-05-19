65bf8cfb4bfebbd35d8b7dd039a717b4e10ec66c
tomasmecius <tmecius@gmail.com>
2021-05-19 10:33:45 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
+import pathlib
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(name='Calculator',
 version='0.1',
@@ -7,6 +16,6 @@ url='https://github.com/mmecius/calculator',
 author='Tomas',
 author_email='tmecius@gmail.com',
 license='MIT',
-install_reqs = ['requirements.txt'],
+install_requires=install_requires,
 packages=['calculator'],
 zip_safe=False)
