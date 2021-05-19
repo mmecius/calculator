713c26931916b65fbff46ecec4512ea54171c8d2
tomasmecius <tmecius@gmail.com>
2021-05-19 10:27:01 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='Calculator',
 version='0.1',
 description='calculator with basic operations',
-url='#',
+url='https://github.com/mmecius/calculator',
 author='Tomas',
 author_email='tmecius@gmail.com',
 license='MIT',
-install_requires = ['requirements.txt'],
+install_reqs = ['requirements.txt'],
 packages=['calculator'],
 zip_safe=False)
